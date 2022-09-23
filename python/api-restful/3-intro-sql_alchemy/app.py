ef728b8cd836ee6360273bcd62db6f03d2744375
danielscheicher@gmail.com <danielscheicher@gmail.com>
2022-09-23 12:01:51 -0300

    last commit in 3-intro-sql_alchemy folder

--- python/api-restful/3-intro-sql_alchemy/app.py
+++ python/api-restful/3-intro-sql_alchemy/app.py
@@ -1,18 +1,42 @@
+# ----- realizando imports -----
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 
+# ----- configuracoes para objeto do tipo Flask -----
 app = Flask(__name__)
+# 1. configurando o data_base_alchemy do nosso objeto app do tipo Flask para um sqlite (podendo ser também postgre, mysql, etc)
+# 1. para o sql_alchemy criar no banco para nós
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+# 1. para não ficar sobrecarrregando nossa aplicacao (rastreamento de modificacoes irá continuar sendo feito pelo alchemy mesmo como false)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # criando variável que irá inicializar aplicação flask
 api = Api(app)
 
+
+# ----- decorador que indica o que fazer antes da primeira requisicao ao app -----
+# 1. criando decorador
+@app.before_first_request
+# 1. criando funcao para rodar, caso decorador seja ativado
+def cria_banco():
+    # 1. antes da primeira requisicao, o banco será criado
+    banco.create_all()
+
+
+# ----- código de fato -----
 # adding a resource to the api
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
-# if the application is not imported and running by itself
+
+# ----- configuracoes para quando o arquivo é executado como main -----
+# if the application is not imported, but it's running by itself
 if __name__ == '__main__':
+    # 1. estamos importando o objeto banco criado no arquivo sql_alchemy
+    from sql_alchemy import banco
+    # 1. irá inicializar o banco quando o app for inicializado
+    banco.init_app(app)
     # Debug true só enquanto estiver produzindo. Depois, colocar como False
     app.run(debug=True)
 
--- python/api-restful/3-intro-sql_alchemy/models/hotel.py
+++ python/api-restful/3-intro-sql_alchemy/models/hotel.py
@@ -1,4 +1,22 @@
-class HotelModel:
+# ----- imports -----
+# 1. importando obj banco criado em sql_alchemy
+from sql_alchemy import banco
+
+
+# 1. ----- nossa classe hotel agora será do tipo Model ----- 
+class HotelModel(banco.Model):
+
+
+    # 1. ----- configurando nome da tabela ----- 
+    __tablename__ = 'hoteis'
+
+    # 1. ----- configurar cada atributo da nossa classe como coluna ----- 
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
+
     # definindo construtor; é aconselhavel criar, posteriormente, getters e setters
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade) -> None:
         self.hotel_id = hotel_id

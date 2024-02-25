954f3b2a37b9567c093193578c2fd82f15217ae8
DanScherr <67443601+DanScherr@users.noreply.github.com>
2024-02-25 18:46:44 -0300

    Update Functional

--- spring-boot/vivo-intro/functional-programing/Functional.java
+++ spring-boot/vivo-intro/functional-programing/Functional.java
@@ -2,7 +2,10 @@ import java.util.List;
 
 class Functional {
     public static void main(String[] args) {
-        printAllNumberInStructured(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+        // printAllNumberInStructured(numbers);
+        // printEvenNumbersInStructured(numbers);
+        printSquareOfEvenNumbersInStructured(numbers);
     }
 
     // Common way
@@ -15,11 +18,45 @@ class Functional {
 
     // Functional way
     private static void printAllNumberInStructured(List<Integer> numbers) {
+        // numbers.stream()
+        //     .forEach(Functional::print); // use method reference nameOfClass::nameOfMethod
+
         numbers.stream()
-            .forEach(Functional::print); // use method reference nameOfClass::nameOfMethod
+            .forEach(System.out::println); // className::staticMethod
     }
 
-    public static void print(int number) {
-        System.out.println(number);
+    private static void printEvenNumbersInStructured(List<Integer> numbers) {
+        // numbers.stream()
+                // Filter
+                // .filter(Functional::isEven)
+        //     .forEach(Functional::print); // use method reference nameOfClass::nameOfMethod
+
+        numbers.stream()
+            // Filter
+            .filter(number -> number%2==0) // using lambda expression - simpler way to define method
+            .forEach(System.out::println); // className::staticMethod
     }
+
+    private static void printSquareOfEvenNumbersInStructured(List<Integer> numbers) {
+        // numbers.stream()
+                // Filter
+                // .filter(Functional::isEven)
+        //     .forEach(Functional::print); // use method reference nameOfClass::nameOfMethod
+
+        numbers.stream()
+            // Filter
+            .filter(number -> number%2==0) // using lambda expression - simpler way to define method
+            .map(number -> number*number)
+            .forEach(System.out::println); // className::staticMethod
+    }
+
+    // public static void print(int number) {
+    //     System.out.println(number);
+    // }
+
+    // private static boolean isEven(int number) {
+    //     return number%2==0;
+    // }
+
+    
 }
